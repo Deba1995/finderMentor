2af4345fef3f81586bd863195ff3753801a895d8
Deba Dey <debadey886@gmail.com>
2024-04-07 22:46:26 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -26,7 +26,7 @@ app.use(express.json({ limit: '5mb' }))
 app.use(express.urlencoded({ limit: '5mb', extended: true }))
 
 // Allows our React application to make HTTP requests to Express application
-app.use(cors())
+//app.use(cors())
 
 /**
  * -------------- ROUTES ----------------
